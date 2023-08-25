a24fa4461fc2c6f86b144172b3ed3ba7ab823900
Charles <charles.jouenne@gmail.com>
2023-08-25 16:54:49 +0200

    Récupération des champs du formulaire

--- modal.js
+++ modal.js
@@ -10,8 +10,10 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const formData = document.querySelectorAll(".formData > input");
 const closeBtn = document.querySelector(".close");
+const form = document.querySelector("form");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -23,10 +25,15 @@ function launchModal() {
 
 // close modal form
 function closeModal() {
-  modalbg.style += "block";
+  modalbg.style.display = "none";
 }
 
 // close modal event
-closeBtn.addEventListener("click", () => {
-  closeModal ()
-})
+closeBtn.addEventListener("click", closeModal)
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  console.log(formData)
+  const res = Array.from(formData).map(item => {return item.value})
+  console.log(res)
+});
